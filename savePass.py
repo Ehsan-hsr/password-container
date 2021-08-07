104a2d9cebacc95ea8eaaaf93641aac2cd4b65f7
ehsan <ehsan>
2021-08-07 17:55:17 +0430

    small start

--- savePass.py
+++ savePass.py
@@ -6,14 +6,14 @@ from os.path import isfile
 
 data={"website": ".", "username": ".", "password": "."}
 
-def getData():
+def get_data():
     data={}
     data["website"]=input("Enter your website address:")
     data["username"]=input("Enter your user name:")
     data["password"]=input("Enter your password:")
     return data
-def writeData(data):
-    file=open("data.txt","w")
+def write_data(data):
+    file=open("data.txt")
     jdata=json.load(file)
     jdata["sites"].append(data)
     file.close()
@@ -21,38 +21,31 @@ def writeData(data):
     json.dump(jdata,file)
     file.close()
 
-def readData(website=None):
-    try:
-        file=open("data.txt","r")
-        sites=json.load(file)
-        file.close()
-        if(website):
-            for site in sites["sites"]:
-                 if site["website"]==website:
-                     return site
-            else:
-                 return sites
-    except:
-        print("couldnt open file")
-        return []
+def read_data(website=None):
+    file=open("data.txt","r")
+    sites=json.load(file)
+    file.close()
+    if(website):
+        for site in sites["sites"]:
+             if site["website"]==website:
+                 return site
+    else:
+         return sites
 
-
-def printResult(data):
+def print_result(data):
     for x in data:
         print(x,":",data[x])
         print("")
     print("<<<<job successfully done>>>>")
 
-
-
-def askName():
+def ask_name():
     siteName=input("enter website name:")
-    sites=readData()
+    sites=read_data()
     siteNames=[]
-    for site in sites["sites"]:
-         siteNames.append(site["website"])
-    
-    if len(get_close_matches(siteName,siteNames) and len(sites)>0)>0:
+    if sites:
+        for site in sites["sites"]:
+             siteNames.append(site["website"])
+    if len(get_close_matches(siteName,siteNames))>0:
         yn=input("did you mean %s ?y/n   "%get_close_matches(siteName,siteNames)[0])
         if (yn=="y"):
              return {"status":"200","data":get_close_matches(siteName,siteNames)[0]}
@@ -68,19 +61,23 @@ def askName():
 def file_exist():
     return isfile("data.txt")
 
+def creat_container():
+    sites={"sites":[]}
+    file=open("data.txt",'w+')
+    jstring=json.dumps(sites)
+    file.write(jstring)
+    file.close()
 
 #check data.txt file exist
 if not file_exist():
-    q=input("data.txt file are not available .create a new one?y/n  ")
-    if q=="y":
-            writeData(data)
-
+    creat_container()   
 #this is main loop
 while(True):
-    site=askName()
+    site=ask_name()
     if (site["status"]=="200"):
-        printResult(readData(site["data"]))
+        print_result(read_data(site["data"]))
     elif (site["status"]=="202") :
-        writeData(getData())
+        write_data(get_data())
+        print("your data stored successfully")
 
 
